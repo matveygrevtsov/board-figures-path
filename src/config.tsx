6e207e87fc2104b152f80b52674649592b70dee4
matveygrevtsov <matveygrevcov@gmail.com>
2024-04-28 22:31:42 +0300

    fix

--- src/config.tsx
+++ src/config.tsx
@@ -1,11 +1,17 @@
-import { AppleOutlined, MehOutlined, MoonOutlined } from "@ant-design/icons";
+import {
+  CaretDownOutlined,
+  MehOutlined,
+  MoonOutlined,
+} from "@ant-design/icons";
 import { ReactNode } from "react";
+
+export const iterationCountLimit = 100000;
 
 export const boardSizes: number[] = [2, 3, 4, 5, 6];
 
 export const figures: ReactNode[] = [
   <MoonOutlined />,
-  <AppleOutlined />,
+  <CaretDownOutlined />,
   <MehOutlined />,
 ];
 
--- src/utils/getAnswer.ts
+++ src/utils/getAnswer.ts
@@ -1,10 +1,10 @@
+import { iterationCountLimit } from "../config";
 import { ICompletedCell, IVisitedGraphNode } from "../types";
 import { buildGraph } from "./buildGraph";
+import { getNodeOutputs } from "./getNodeOutputs";
 
 export const getAnswer = (cells: ICompletedCell[]): number[] | undefined => {
   const graph = buildGraph(cells);
-
-  console.log(graph);
 
   for (let i = 0; i < graph.length; i++) {
     const startNode = graph[i];
@@ -16,23 +16,30 @@ export const getAnswer = (cells: ICompletedCell[]): number[] | undefined => {
       },
     ];
 
+    let iterationCount = 0;
+
     while (visitedNodes.length) {
+      if (iterationCount === iterationCountLimit) {
+        console.error(visitedNodes);
+        return undefined;
+      }
+
       if (visitedNodes[0].input.length === cells.length) {
-        console.log(visitedNodes[0].input);
         return visitedNodes[0].input;
       }
 
       // Отсеиваем те дочерние вершины, которые уже встречались по пути к этой.
-      const outputs: IVisitedGraphNode[] = visitedNodes[0].outputs
-        .filter((id) => !visitedNodes[0].input.includes(id))
-        .map((id) => ({
-          ...graph[id],
-          input: [...visitedNodes[0].input, id],
-        }));
+      const outputs = getNodeOutputs(
+        visitedNodes[0],
+        Math.sqrt(cells.length),
+        graph
+      );
 
       visitedNodes.shift();
 
       visitedNodes.push(...outputs);
+
+      ++iterationCount;
     }
   }
 
