8094f3a01a648ce34cf15048f3c441f42180e0b5
matveygrevtsov <matveygrevcov@gmail.com>
2024-05-11 11:01:56 +0300

    fix 2

--- src/utils/getNodeOutputs.ts
+++ src/utils/getNodeOutputs.ts
@@ -1,65 +1,11 @@
 import { IGraphNode, IVisitedGraphNode } from "../types";
-import { mapCellIdToTableCoordinates } from "./mapCellIdToTableCoordinates";
 
 export const getNodeOutputs = (
   node: IVisitedGraphNode,
-  tableSize: number,
   graph: IGraphNode[]
 ): IVisitedGraphNode[] => {
   return node.outputs
-    .filter((id) => {
-      // Если вершина уже встречалась на пути - не рассматриваем её.
-      if (node.input.includes(id)) {
-        return false;
-      }
-
-      if (node.input.length === 1) {
-        return true;
-      }
-
-      const [nextRowIndex, nextColumnIndex] = mapCellIdToTableCoordinates(
-        id,
-        tableSize
-      );
-
-      const [prevRowIndex, prevColumnIndex] = mapCellIdToTableCoordinates(
-        node.input[node.input.length - 1],
-        tableSize
-      );
-
-      const [prevPrevRowIndex, prevPrevColumnIndex] =
-        mapCellIdToTableCoordinates(
-          node.input[node.input.length - 2],
-          tableSize
-        );
-
-      if (nextRowIndex === prevRowIndex && prevRowIndex === prevPrevRowIndex) {
-        // Движение вправо.
-        if (prevColumnIndex > prevPrevColumnIndex) {
-          return nextColumnIndex > prevColumnIndex;
-        }
-        // Движение влево.
-        else {
-          return nextColumnIndex < prevColumnIndex;
-        }
-      }
-
-      if (
-        nextColumnIndex === prevColumnIndex &&
-        prevColumnIndex === prevPrevColumnIndex
-      ) {
-        // Движение вверх.
-        if (prevRowIndex > prevPrevRowIndex) {
-          return nextRowIndex > prevRowIndex;
-        }
-        // Движение вниз.
-        else {
-          return nextRowIndex < prevRowIndex;
-        }
-      }
-
-      return true;
-    })
+    .filter((id) => !node.input.includes(id)) // Если вершина уже встречалась на пути - не рассматриваем её.
     .map((id) => ({
       ...graph[id],
       input: [...node.input, id],
